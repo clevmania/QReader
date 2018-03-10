892459e6b77bbe44d2a2b6ce2f8df32a607f0aae
lawrence <lawrence@grandilo.com>
2018-03-10 10:27:16 +0100

    variable declaration

--- app/src/main/java/com/clevmania/qreader/MainActivity.java
+++ app/src/main/java/com/clevmania/qreader/MainActivity.java
@@ -2,8 +2,18 @@ package com.clevmania.qreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    private TextView renderedQrCode;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int PermissionID = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
