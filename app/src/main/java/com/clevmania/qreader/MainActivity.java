ef1e36567ee76028093ac6c46b2f028d86940515
lawrence <lawrence@grandilo.com>
2018-03-10 10:37:20 +0100

    initialised views

--- app/src/main/java/com/clevmania/qreader/MainActivity.java
+++ app/src/main/java/com/clevmania/qreader/MainActivity.java
@@ -1,12 +1,19 @@
 package com.clevmania.qreader;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     private SurfaceView surfaceView;
@@ -19,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        renderedQrCode = findViewById(R.id.tv_decoded_result);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480).build();
+
+
     }
 }
